f66626dcfce443fff21d7f085c9af7a21ade0191
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-10-04 06:20:27 +1100

    Added req option to not try and cut out (potentially unfinished) last word

--- request_sender.py
+++ request_sender.py
@@ -1,14 +1,15 @@
 import requests
 import json
 
-#Note: this is likely to truncate the final word of a sentence
+#Basic Request Translation
 def request_translation(text):
     json_content = {
         'text': text,
         'index': 0,
+        'truncate': False
     }
 
     print(f"Translating: {text}")
     response = requests.get("http://127.0.0.1:5000/parse_text", json=json.dumps(json_content))
-    print(f"Response: {response.json()}")
-    return response.json()
+    print(f"Response: {response.json()['text']}")
+    return response.json()['text']
