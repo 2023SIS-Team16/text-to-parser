564154b9b8839bf8cae695784114bc2157b73eec
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-10-11 09:15:54 +1100

    Request_dicts for local communication

---
+++ request_dicts.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+#Returns string
+def request_translation(text):
+    json_content = {
+        'text': text,
+        'index': 0,
+        'truncate': False
+    }
+
+    print(f"Translating: {text}")
+    response = requests.get("http://127.0.0.1:5000/parse_text", json=json.dumps(json_content))
+    print(f"Response: {response.json()['text']}")
+
+    return response.text
+
+request_translation("TEST")
